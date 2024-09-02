5642b3be34471a5ede5f7ccf5d40578774bbb4c9
Gabriel Silva <gabrieglpsi@gmail.com>
2024-09-01 21:47:18 -0300

    Teste usuario

--- banco.sql
+++ banco.sql
@@ -16,3 +16,12 @@ CREATE TABLE usuario (
     senha VARCHAR(255) NOT NULL,
     data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+
+CREATE TABLE usuarioEmString (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    senha VARCHAR(255) NOT NULL,
+    data_criacao VARCHAR DEFAULT TIMESTAMP
+);
