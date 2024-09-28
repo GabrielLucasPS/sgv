9f67f9401a74b6731f12777a01596380b1ffd613
GabrielLucasPS <gabrielglpsi@gmail.com>
2024-09-28 18:45:33 -0300

    Ajustes CSS

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,7 @@ export default async function RootLayout({
             <body
                 className={`${geistSans.variable} ${geistMono.variable} antialiased`}
             >
-                <div className="flex min-h-screen overflow-hidden">
+                <div className="flex min-h-screen max-h-screen overflow-hidden">
                     {!!session && <Navbar></Navbar>}
                     {!session && <></>}
 
--- app/usuarios/page.tsx
+++ app/usuarios/page.tsx
@@ -9,15 +9,13 @@ export default async function Usuarios() {
     const session = await getServerSession();
     return (
         <main className="usuarios_page">
-            <h1 className="sectionTitle">Usuario:</h1>
-
-            <div>
-                <h3>Adicionar Usuario</h3>
+            <div className="adicionarUsuarios">
+                <h2 className="font-bold text-2xl mb-2">Adicionar Usuario</h2>
                 <CreateUserForm />
             </div>
 
             <div>
-                <h3>Usuarios</h3>
+                <h2 className="font-bold text-2xl mb-2">Usuarios</h2>
                 <TabelaUsuarios session={session} />
             </div>
         </main>
