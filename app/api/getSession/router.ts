2dc6d6f436816fa48c9878727995c9a7f056b4fd
Gabriel Silva <gabrieglpsi@gmail.com>
2024-09-28 18:22:37 -0300

    ReStart

--- app/api/getSession/router.ts
+++
@@ -1,8 +0,0 @@
-import { JWTInterface } from "@/app/types";
-import { getSession } from "@/lib";
-
-export async function POST(req: Request) {
-    const session: JWTInterface = await getSession();
-
-    return session.user;
-}
