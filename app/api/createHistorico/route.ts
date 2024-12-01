180df4a3706401d316e5937b850a992ea2634fc7
Gabriel <gabriel.silva@gruposetta.com>
2024-12-01 15:40:48 -0300

    versão expotech

--- app/api/createHistorico/route.ts
+++ app/api/createHistorico/route.ts
@@ -7,114 +7,114 @@ import { revalidatePath } from "next/cache";
 import { Animal, Vacina } from "@/lib/types/dbTypes";
 
 export const FormSchema = z.object({
-    animalId: z.number(),
-    vacinaId: z.number(),
-    dataVacinacao: z.any(),
-    dosagem: z.number(),
-    observacao: z.string().max(250).optional(),
+	animalId: z.number(),
+	vacinaId: z.number(),
+	dataVacinacao: z.any(),
+	dosagem: z.number(),
+	observacao: z.string().max(250).optional(),
 });
 
 export async function POST(req: Request) {
-    try {
-        const body = await req.json();
-        const { animalId, vacinaId, dataVacinacao, dosagem, observacao } =
-            FormSchema.parse(body);
+	try {
+		const body = await req.json();
+		const { animalId, vacinaId, dataVacinacao, dosagem, observacao } =
+			FormSchema.parse(body);
 
-        if (animalId && vacinaId && dataVacinacao && dosagem) {
-            const isoTimestamp = subHours(new Date().toISOString(), 0);
-            let intervalodose = 0;
-            let inicioDataVacinaca = startOfDay(dataVacinacao);
-            const animalExistente = await pool.query(
-                `SELECT id, brinco FROM animal WHERE id = $1 LIMIT 1`,
-                [animalId]
-            );
+		if (animalId && vacinaId && dataVacinacao && dosagem) {
+			const isoTimestamp = subHours(new Date().toISOString(), 0);
+			let intervalodose = 0;
+			let inicioDataVacinaca = startOfDay(dataVacinacao);
+			const animalExistente = await pool.query(
+				`SELECT id, brinco FROM animal WHERE id = $1 LIMIT 1`,
+				[animalId]
+			);
 
-            if (animalExistente.rows.length > 0) {
-            } else {
-                return NextResponse.json(
-                    {
-                        success: false,
-                        message: "Animal não encontrada",
-                    },
-                    { status: 400 }
-                );
-            }
+			if (animalExistente.rows.length > 0) {
+			} else {
+				return NextResponse.json(
+					{
+						success: false,
+						message: "Animal não encontrada",
+					},
+					{ status: 400 }
+				);
+			}
 
-            const vacinaExistente = await pool.query(
-                `SELECT id, intervalodose FROM vacina WHERE id = $1 LIMIT 1`,
-                [vacinaId]
-            );
+			const vacinaExistente = await pool.query(
+				`SELECT id, intervalodose FROM vacina WHERE id = $1 LIMIT 1`,
+				[vacinaId]
+			);
 
-            if (vacinaExistente.rows.length > 0) {
-                intervalodose = vacinaExistente.rows[0].intervalodose;
-            } else {
-                return NextResponse.json(
-                    {
-                        success: false,
-                        message: "Vacina não encontrada.",
-                    },
-                    { status: 400 }
-                );
-            }
+			if (vacinaExistente.rows.length > 0) {
+				intervalodose = vacinaExistente.rows[0].intervalodose;
+			} else {
+				return NextResponse.json(
+					{
+						success: false,
+						message: "Vacina não encontrada.",
+					},
+					{ status: 400 }
+				);
+			}
 
-            const historicoExistente = await pool.query(
-                `SELECT animal_id, vacina_id FROM historicovacina WHERE animal_id = $1 AND vacina_id = $2 LIMIT 1`,
-                [animalId, vacinaId]
-            );
+			const historicoExistente = await pool.query(
+				`SELECT animal_id, vacina_id FROM historicovacina WHERE animal_id = $1 AND vacina_id = $2 LIMIT 1`,
+				[animalId, vacinaId]
+			);
 
-            if (historicoExistente.rows.length > 0) {
-                return NextResponse.json(
-                    {
-                        success: false,
-                        message: "Historico já existe.",
-                    },
-                    { status: 400 }
-                );
-            }
+			if (historicoExistente.rows.length > 0) {
+				return NextResponse.json(
+					{
+						success: false,
+						message: "Historico já existe.",
+					},
+					{ status: 400 }
+				);
+			}
 
-            var newInicioDataVacinaca = startOfDay(inicioDataVacinaca);
-            await pool.query(
-                `INSERT INTO historicovacina(animal_id, vacina_id, data_vacinacao, dosagem, intervalo_proxima_dose, observacao,dataultimavacina)
-                 VALUES ($1, $2, $3, $4, $5, $6)
+			var newInicioDataVacinaca = startOfDay(inicioDataVacinaca);
+			await pool.query(
+				`INSERT INTO historicovacina(animal_id, vacina_id, data_vacinacao, dosagem, intervalo_proxima_dose, observacao,dataultimavacina)
+                 VALUES ($1, $2, $3, $4, $5, $6, $7)
                  RETURNING *`,
-                [
-                    animalId,
-                    vacinaId,
-                    newInicioDataVacinaca,
-                    dosagem,
-                    intervalodose,
-                    observacao,
-                    newInicioDataVacinaca,
-                ]
-            );
-            console.log("Historico adicionado.");
+				[
+					animalId,
+					vacinaId,
+					newInicioDataVacinaca,
+					dosagem,
+					intervalodose,
+					observacao,
+					newInicioDataVacinaca,
+				]
+			);
+			console.log("Historico adicionado.");
 
-            return NextResponse.json(
-                {
-                    success: true,
-                    message: "Historico adicionado.",
-                },
-                { status: 201 }
-            );
-        } else {
-            return NextResponse.json(
-                {
-                    success: false,
-                    message: "Não foi possivel adicionar historico.",
-                },
-                { status: 400 }
-            );
-        }
-    } catch (error) {
-        console.log(error);
-        return NextResponse.json(
-            {
-                success: false,
-                message: "Erro ao processar a requisição.",
-            },
-            { status: 500 }
-        );
-    }
+			return NextResponse.json(
+				{
+					success: true,
+					message: "Historico adicionado.",
+				},
+				{ status: 201 }
+			);
+		} else {
+			return NextResponse.json(
+				{
+					success: false,
+					message: "Não foi possivel adicionar historico.",
+				},
+				{ status: 400 }
+			);
+		}
+	} catch (error) {
+		console.log(error);
+		return NextResponse.json(
+			{
+				success: false,
+				message: "Erro ao processar a requisição.",
+			},
+			{ status: 500 }
+		);
+	}
 }
 // INSERT INTO public.historicovacina(
 // 	id, animal_id, vacina_id, data_vacinacao, dosagem, intervalo_proxima_dose, status, observacao, responsavel_id)
--- app/componentes/ListaVacinasComHistoricos/ListaVacinasComHistoricos.tsx
+++ app/componentes/ListaVacinasComHistoricos/ListaVacinasComHistoricos.tsx
@@ -7,13 +7,13 @@ import { addDays, format, set } from "date-fns";
 import { useDraggable } from "react-use-draggable-scroll";
 import AnimalDoHistorico from "../animalDoHistorico/animalDoHistorico";
 import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogFooter,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
+	Dialog,
+	DialogContent,
+	DialogDescription,
+	DialogFooter,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
 } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
 import { Label } from "@radix-ui/react-label";
@@ -21,197 +21,197 @@ import { Input } from "@/components/ui/input";
 import { useToast } from "@/hooks/use-toast";
 
 type Vacina = {
-    id: number;
-    nome: string;
-    descricao?: string;
-    intervalodose: number;
-    fabricante: string;
-    dataregistro?: Date;
+	id: number;
+	nome: string;
+	descricao?: string;
+	intervalodose: number;
+	fabricante: string;
+	dataregistro?: Date;
 };
 
 interface Props {
-    vacina: Vacina;
+	vacina: Vacina;
 }
 
 const ListaVacinasComHistoricos: React.FC<Props> = ({ vacina }) => {
-    const { toast } = useToast();
-    const ref =
-        useRef<HTMLDivElement>() as React.MutableRefObject<HTMLInputElement>;
-    const { events } = useDraggable(ref, {
-        applyRubberBandEffect: true,
-    });
+	const { toast } = useToast();
+	const ref =
+		useRef<HTMLDivElement>() as React.MutableRefObject<HTMLInputElement>;
+	const { events } = useDraggable(ref, {
+		applyRubberBandEffect: true,
+	});
 
-    let novaDataInicial = new Date();
-    const [novaData, setNovaData] = useState<Date>(novaDataInicial);
+	let novaDataInicial = new Date();
+	const [novaData, setNovaData] = useState<Date>(novaDataInicial);
 
-    const handleDateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const selectedDate = e.target.value;
-        setNovaData(new Date(selectedDate));
-    };
+	const handleDateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const selectedDate = e.target.value;
+		setNovaData(new Date(selectedDate));
+	};
 
-    const handleNovaData = async (data: Date, id: number) => {
-        const sucesso = await atualizarDataHistorico(data, id);
-        if (sucesso == true) {
-        } else {
-            console.error("Falha na atualização.");
-        }
-    };
+	const handleNovaData = async (data: Date, id: number) => {
+		const sucesso = await atualizarDataHistorico(data, id);
+		if (sucesso == true) {
+		} else {
+			console.error("Falha na atualização.");
+		}
+	};
 
-    const [historicosPorVacina, setHistoricosPorVacina] = useState<
-        HistoricoVacina[]
-    >([]);
+	const [historicosPorVacina, setHistoricosPorVacina] = useState<
+		HistoricoVacina[]
+	>([]);
 
-    const getHistoricos = async () => {
-        const newHistoricos = getHistoricosPorVacina(vacina.id);
-        setHistoricosPorVacina(await newHistoricos);
-    };
+	const getHistoricos = async () => {
+		const newHistoricos = getHistoricosPorVacina(vacina.id);
+		setHistoricosPorVacina(await newHistoricos);
+	};
 
-    useEffect(() => {
-        getHistoricos();
-    }, []);
+	useEffect(() => {
+		getHistoricos();
+	}, []);
 
-    return (
-        <div>
-            <div key={vacina.id} className="VacinasContainer">
-                <div className="vacinaContainer">
-                    <h2>
-                        <strong className="text-[1.2vw] ">{vacina.nome}</strong>
-                    </h2>
-                    <p className="mb-2 overflow-auto max-h-[50%]">
-                        <strong>Descrição:</strong>
-                        {vacina.descricao || "Sem descrição disponível"}
-                    </p>
-                    <p>
-                        <strong>Fabricante:</strong> {vacina.fabricante}
-                    </p>
-                    <h3>Históricos:</h3>
-                </div>
+	return (
+		<div>
+			<div key={vacina.id} className="VacinasContainer">
+				<div className="vacinaContainer ">
+					<h2>
+						<strong className="text-[1.2vw] ">{vacina.nome}</strong>
+					</h2>
+					<p className="mb-2 overflow-y-auto overflow-x-hidden break-words max-h-[50%]">
+						<strong>Descrição:</strong>
+						{vacina.descricao || "Sem descrição disponível"}
+					</p>
+					<p>
+						<strong>Fabricante:</strong> {vacina.fabricante}
+					</p>
+					<h3>Históricos:</h3>
+				</div>
 
-                <div className="historicosListContainer " ref={ref} {...events}>
-                    {historicosPorVacina.length ? (
-                        historicosPorVacina.map((historico) => (
-                            <div
-                                className="historicoContainer cursor-pointer hover:scale-105 lg:hover:scale-[1.01] transition duration-200"
-                                key={historico.id}
-                            >
-                                <div className="historicoTitle">
-                                    <div>
-                                        <AnimalDoHistorico
-                                            historico={historico}
-                                        />
-                                    </div>
-                                    <div className="flex items-center">
-                                        <Dialog>
-                                            <DialogTrigger>
-                                                <Button className="editIcon"></Button>
-                                            </DialogTrigger>
-                                            <DialogContent className="sm:max-w-[425px]">
-                                                <DialogHeader>
-                                                    <DialogTitle>
-                                                        Selecione a data que foi
-                                                        vacinado
-                                                    </DialogTitle>
-                                                </DialogHeader>
-                                                <div className="grid gap-4 py-4">
-                                                    <div className="flex flex-col items-center justify-start gap-2">
-                                                        <Label
-                                                            htmlFor="name"
-                                                            className="text-left w-full "
-                                                        >
-                                                            Data da vacinação
-                                                        </Label>
-                                                        <input
-                                                            id="date"
-                                                            className="w-full text-[1vw] inputNovaDataVacinacao rounded-md"
-                                                            type="date"
-                                                            onChange={
-                                                                handleDateChange
-                                                            }
-                                                            value={
-                                                                novaData
-                                                                    .toISOString()
-                                                                    .split(
-                                                                        "T"
-                                                                    )[0]
-                                                            }
-                                                        />
-                                                    </div>
-                                                </div>
-                                                <DialogFooter>
-                                                    <Button
-                                                        onClick={() =>
-                                                            handleNovaData(
-                                                                novaData,
-                                                                historico.id
-                                                            )
-                                                        }
-                                                        className="btn"
-                                                    >
-                                                        Salvar
-                                                    </Button>
-                                                </DialogFooter>
-                                            </DialogContent>
-                                        </Dialog>
-                                        <div
-                                            className={
-                                                addDays(
-                                                    new Date(
-                                                        historico.dataultimavacina
-                                                    ),
-                                                    historico.intervalo_proxima_dose
-                                                        ? historico.intervalo_proxima_dose
-                                                        : 0
-                                                ) >= new Date()
-                                                    ? "historicoStatus status-realizada ml-4"
-                                                    : "historicoStatus status-atrasada ml-4"
-                                            }
-                                        ></div>
-                                    </div>
-                                </div>
-                                <div className="historicoContent">
-                                    <div className="contentText">
-                                        <span>Dosagem:</span>
-                                        <span>{historico.dosagem} ml</span>
-                                    </div>
+				<div className="historicosListContainer " ref={ref} {...events}>
+					{historicosPorVacina.length ? (
+						historicosPorVacina.map((historico) => (
+							<div
+								className="historicoContainer cursor-pointer hover:scale-105 lg:hover:scale-[1.01] transition duration-200"
+								key={historico.id}
+							>
+								<div className="historicoTitle">
+									<div>
+										<AnimalDoHistorico
+											historico={historico}
+										/>
+									</div>
+									<div className="flex items-center">
+										<Dialog>
+											<DialogTrigger>
+												<Button className="editIcon"></Button>
+											</DialogTrigger>
+											<DialogContent className="sm:max-w-[425px]">
+												<DialogHeader>
+													<DialogTitle>
+														Selecione a data que foi
+														vacinado
+													</DialogTitle>
+												</DialogHeader>
+												<div className="grid gap-4 py-4">
+													<div className="flex flex-col items-center justify-start gap-2">
+														<Label
+															htmlFor="name"
+															className="text-left w-full "
+														>
+															Data da vacinação
+														</Label>
+														<input
+															id="date"
+															className="w-full text-[1vw] inputNovaDataVacinacao rounded-md"
+															type="date"
+															onChange={
+																handleDateChange
+															}
+															value={
+																novaData
+																	.toISOString()
+																	.split(
+																		"T"
+																	)[0]
+															}
+														/>
+													</div>
+												</div>
+												<DialogFooter>
+													<Button
+														onClick={() =>
+															handleNovaData(
+																novaData,
+																historico.id
+															)
+														}
+														className="btn"
+													>
+														Salvar
+													</Button>
+												</DialogFooter>
+											</DialogContent>
+										</Dialog>
+										<div
+											className={
+												addDays(
+													new Date(
+														historico.dataultimavacina
+													),
+													historico.intervalo_proxima_dose
+														? historico.intervalo_proxima_dose
+														: 0
+												) >= new Date()
+													? "historicoStatus status-realizada ml-4"
+													: "historicoStatus status-atrasada ml-4"
+											}
+										></div>
+									</div>
+								</div>
+								<div className="historicoContent">
+									<div className="contentText">
+										<span>Dosagem:</span>
+										<span>{historico.dosagem} ml</span>
+									</div>
 
-                                    <div className="contentText">
-                                        <span>Proxíma dose:</span>
-                                        <span>
-                                            {format(
-                                                addDays(
-                                                    new Date(
-                                                        historico.dataultimavacina
-                                                    ),
-                                                    historico.intervalo_proxima_dose
-                                                        ? historico.intervalo_proxima_dose
-                                                        : 0
-                                                ),
-                                                "dd-MM-yyyy"
-                                            )}
-                                        </span>
-                                    </div>
+									<div className="contentText">
+										<span>Proxíma dose:</span>
+										<span>
+											{format(
+												addDays(
+													new Date(
+														historico.dataultimavacina
+													),
+													historico.intervalo_proxima_dose
+														? historico.intervalo_proxima_dose
+														: 0
+												),
+												"dd-MM-yyyy"
+											)}
+										</span>
+									</div>
 
-                                    <div className="contentText">
-                                        <span>Intervalo entre doses:</span>
-                                        <span>
-                                            {historico.intervalo_proxima_dose}{" "}
-                                            Dias
-                                        </span>
-                                    </div>
+									<div className="contentText">
+										<span>Intervalo entre doses:</span>
+										<span>
+											{historico.intervalo_proxima_dose}{" "}
+											Dias
+										</span>
+									</div>
 
-                                    <div className="observacaoContent">
-                                        Observação: {historico.observacao}
-                                    </div>
-                                </div>
-                            </div>
-                        ))
-                    ) : (
-                        <p>Sem históricos para esta vacina.</p>
-                    )}
-                </div>
-            </div>
-        </div>
-    );
+									<div className="observacaoContent">
+										Observação: {historico.observacao}
+									</div>
+								</div>
+							</div>
+						))
+					) : (
+						<p>Sem históricos para esta vacina.</p>
+					)}
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default ListaVacinasComHistoricos;
